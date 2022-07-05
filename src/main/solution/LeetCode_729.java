494b2b2800064a56a0c0c9524cbb25f51cecc068
xycui <xycui@apac.freewheel.com>
2022-07-05 23:32:42 +0800

    LC-729

---
+++ src/main/solution/LeetCode_729.java
@@ -0,0 +1,22 @@
+package main.solution;
+
+import java.util.*;
+
+public class LeetCode_729 {
+    List<int[]> booked;
+
+    public void MyCalendar() {
+        booked = new ArrayList<int[]>();
+    }
+
+    public boolean book(int start, int end) {
+        for (int[] arr : booked) {
+            int l = arr[0], r = arr[1];
+            if (l < end && start < r) {
+                return false;
+            }
+        }
+        booked.add(new int[]{start, end});
+        return true;
+    }
+}
